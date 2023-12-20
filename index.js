eecfb3358e3c4c5368cc47df4858fc284e2551e4
Java <yewshan.ooi@gmail.com>
2023-12-20 22:39:53 +0800

    feat: added bootstrap support and demo toast message

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+// Event to automatically display toast when "Click me" button is clicked
 async function getCatFact() {
     const Fact = await fetch('https://catfact.ninja/fact')
         .then(res => res.json());
 
-    alert(`${Fact.fact}`);
+        var wordCount = Fact.fact.split(" ").length;  
+
+    var element = document.getElementById("myToast");
+    var myToast = new bootstrap.Toast(element, { autohide: false });
+        element.querySelector('.toast-body').innerText = Fact.fact;
+        element.querySelector('.me-small').innerText = wordCount + " words";
+        myToast.show();
 }
+
+// Event to check whether a button is clicked
+document.addEventListener("DOMContentLoaded", function(){
+    var btn = document.getElementById("myBtn");
+    var element = document.getElementById("myToast");
+
+    var myToast = new bootstrap.Toast(element);
+
+    btn.addEventListener("click", function(){
+        myToast.show();
+    });
+});
